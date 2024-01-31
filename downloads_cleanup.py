382e2b069a795731ddfdd2b8849420ada4ce992b
m0brown <mbrown1483@gmail.com>
2024-01-31 17:27:57 -0500

    downloads_cleanup.py initial commit

---
+++ downloads_cleanup.py
@@ -0,0 +1,8 @@
+import os
+
+# File types to be organized
+EXTN_AUDIO = ['.mp3', '.wav', '.flac', '.m4a', '.aac', '.ogg', '.wma']
+EXTN_DOC = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt', '.rtf', '.csv', '.xml', '.json', '.html', '.htm']
+EXTN_IMAGE = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp', '.svg', '.eps', '.raw', '.cr2', '.nef', '.orf', '.sr2']
+EXTN_INSTALLER = ['.exe', '.msi', '.dmg', '.deb', '.rpm', '.apk']
+EXTN_VIDEO = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.vob', '.3gp', '.m4v', '.mpg', '.mpeg']
